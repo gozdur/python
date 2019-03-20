5e7150544e36d95315a599e559281a74cc53a900
gozdur <gozdur.p@gmail.com>
2019-03-20 21:03:20 +0100

    Hello world in Pycharm

--- strings_basic.py
+++ strings_basic.py
@@ -8,7 +8,22 @@ print("Pawel\nGozdur")
 #Print with  \<char> is interpreted s "break" it will print something which would be interpreted otherwise
 
 print("Pawel\"Gozdur")
+print("\n")
 
 # Strings stored in variables and concatenation of strings
+
 phrase = "string in a variable"
 print("\n" + "concatenated " + phrase)
+
+# Usage of functions that can be used on strings example: string.function or variable.function - there are many functions
+
+print(phrase.capitalize()) # takes strings and capitalizes first character
+print(phrase.upper()) # takes strings and capitalizes whole string
+
+
+print(phrase.isupper()) # function will check if characters are uppercase and return boolean True or False
+print(phrase.islower()) # function will check if characters are lowercase and return boolean True or False
+
+# Functions can be also used together
+
+print(phrase.upper().isupper()) # converts a string in phrase variable converts it to uppercase and checks if its true that it is uppercase
