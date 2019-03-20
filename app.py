dc177bd2e545011c2b9f5ce494ec386a5cd455f1
gozdur <gozdur.p@gmail.com>
2019-03-20 19:48:16 +0100

    Hello world in Pycharm

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ name = "Pawel"
 print("My name is " + name + " and, ")
 print("my age is " + age + ".")
 
+# Assign a new value
+
+name = "TOOM"
+
+print("My name is " + name + " and, ")
+print("my age is " + age + ".")
