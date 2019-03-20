49fd7f93b09adbe662431384e6e399f2a02dc8dc
gozdur <gozdur.p@gmail.com>
2019-03-20 21:58:38 +0100

    numbers

--- numbers_basic.py
+++ numbers_basic.py
@@ -1,1 +1,32 @@
-1
+### Basic Numbers in Python
+
+print("Basic printing of numbers")
+print(2) # prints a number
+print(2.1) # prints a decimal number
+print (-2) # prints a negative number
+
+# Basic Arithmetic
+
+print("Basic arithmetic")
+print(2 + 2) # adds two numbers
+print(2 + 2.2) # adds two numbers one is decimal
+print(2 * 2) # multiplies two numbers
+print(2 * 2) # divides two numbers
+print(2 ** 4) # exponents of number
+print(2 * 4 + 4) # standard order of operation
+print(2 * (4 + 4)) # order of operation enforced by parentheses ()
+print(10 % 3) # a mod n in math which gives a reminder like:  dividend a | n divisor
+
+
+# Store numbers in variables
+
+my_var = 5
+my_var2 = 6
+my_var3 = 7
+
+print(my_var) # prints the variable my_var which is a integer (liczba calkowita)
+
+# using function that converts numbers in to a string
+
+print(str(my_var2)) # str function that converts a number is to a string usefull when printing numbers
+print(str(my_var3) + " is my favourite number")
