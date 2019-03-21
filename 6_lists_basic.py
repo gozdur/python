118e130f4568956a4026710800ec68e0728507a5
gozdur <gozdur.p@gmail.com>
2019-03-21 10:09:32 +0100

    List functions

--- 6_lists_basic.py
+++ 6_lists_basic.py
@@ -1,10 +1,16 @@
+# Lists in Python - ARRAY
 
-
-friends = ["Jacek", "Werni", "Nikt"] # variable which is a list
+friends = ["Jacek", "Werni", "Nikt", "Pawel", "Seba"] # variable which is a list
 
 print(friends)  # Will print the list
 
-print(friends[0]) # prints an element of the list with index 0
-print(friends[2]) # prints the third element of the list with index 2
-print(friends[-1]) # negative value starts to count from the back of the list starts with -1 as there is no negative zero
+print(friends[0])   # prints an element of the list with index 0
+print(friends[2])   # prints the third element of the list with index 2
+print(friends[-1])  # negative value starts to count from the back of the list starts with -1 as there is no negative zero
+print(friends[0:])  # will garab first value from the list and all later values
+print(friends[1:])  # will grab second value of the list an all later values
+print(friends[2:5]) # will grab third value from three up to four (will not grap fifth its "up to" - RANGE
 
+friends[1] = "Milosz" # updates the list in the variable at position two
+
+print(friends)
---
+++ 7_lists_functions.py
@@ -0,0 +1,102 @@
+# Fuctions that are used agains lists
+
+
+
+lucky_numbers = [4, 8, 9, 18, 42, 56]
+friends = ["Kevin", "Jack", "Jane", "Oscar", "Toby"]
+
+print(friends)
+print(lucky_numbers)
+
+# Extend function
+
+friends.extend(lucky_numbers)  # adds lits lunky_numbers to the back of the friends list
+print(friends)
+
+# Bring back to original
+
+lucky_numbers = [4, 8, 9, 18, 42, 56] # brings back original list content
+friends = ["Kevin", "Jack", "Jane", "Oscar", "Toby"] # brings back original list content
+
+print(friends) # check
+print(lucky_numbers) # check
+
+# Append function
+
+friends.append("Olek") # .append will add the new value at the end of the list
+print(friends)
+
+# Insert function
+
+friends.insert(1, "Aleksander") # will add a new value on the index position 1 and push other elements to the right
+print(friends)
+
+# Remove function
+
+friends.remove("Olek") # removes a value with a name
+print(friends)
+
+# Clear function
+
+friends.clear()
+print(friends)
+
+# Bring back to original
+
+lucky_numbers = [4, 8, 3, 18, 42, 56] # brings back original list content
+friends = ["Kevin", "Jim", "Jack", "Jane", "Oscar","Jim", "Toby"] # brings back original list content
+
+print(friends) # check
+print(lucky_numbers) # check
+
+# Pop function
+
+friends.pop() # pops last value from the list
+print(friends)
+
+# Index function
+
+print(friends.index("Jane")) # gives the index position of a value specified in the index function
+
+# Count function
+
+print(friends.count("Jim")) # counts how many times a value is present in the list
+
+# Sort Function
+
+friends.sort() # strings are sorted in alphabetical order
+lucky_numbers.sort() # numbers are sorted lowest to highest
+
+print(friends)
+print(lucky_numbers)
+
+# Reverse the order
+
+friends.reverse() # reverses the order of the list but does not sort
+lucky_numbers.reverse() # reverses the order of the list but does not sort
+
+print(friends)
+print(lucky_numbers)
+
+# Copy function
+
+friends2 = friends.copy() # creates a new varialble with a copied frinds list
+print(friends2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
