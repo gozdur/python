39d822d4891581417d7eb6d18520d478a1c3e89e
gozdur <gozdur.p@gmail.com>
2019-03-21 09:16:19 +0100

    Simple calculator complete

---
+++ lists_basic.py
@@ -0,0 +1,10 @@
+
+
+friends = ["Jacek", "Werni", "Nikt"] # variable which is a list
+
+print(friends)  # Will print the list
+
+print(friends[0]) # prints an element of the list with index 0
+print(friends[2]) # prints the third element of the list with index 2
+print(friends[-1]) # negative value starts to count from the back of the list starts with -1 as there is no negative zero
+
