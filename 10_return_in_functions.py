3fa5e24fa3f8452f8359ccc4ca093b10db87f134
gozdur <gozdur.p@gmail.com>
2019-03-21 12:35:51 +0100

    List functions

--- 10_return_in_functions.py
+++ 10_return_in_functions.py
@@ -1,7 +1,16 @@
 
-
+# return statement returns the output of the function, otherwise print
+# stetement would give none, when we use return it records the output
 
 def cube(num):
     return num*num*num
 
-print(cube(3))
+print(cube(4))
+
+
+def add(num):
+    return num+num
+
+
+result = add(3)
+print(result)
