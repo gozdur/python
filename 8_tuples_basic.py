326bc0c3be169fc444c4293d9f1d8b15114d0709
gozdur <gozdur.p@gmail.com>
2019-03-21 10:29:14 +0100

    List functions

---
+++ 8_tuples_basic.py
@@ -0,0 +1,15 @@
+# Tuples are like list but cannot change any value once you create a Tuple
+
+coordinates = (4, 5)
+print(coordinates)
+
+coordinates = (4, 5)
+print(coordinates[0])
+
+
+# Difrence between tuples and lists
+    # Tuples are not motifiable
+    # Lists are made out of square brackets[] and tuples out of parenthasis ()
+
+coordinates_list = [(6, 4), (6, 8, 9)] # this is a list that contains two tuples
+print(coordinates_list)
