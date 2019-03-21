8d2b9e6855ccf66dcfd2bffc7fbf8d2b112dd44f
gozdur <gozdur.p@gmail.com>
2019-03-21 11:00:58 +0100

    List functions

---
+++ 9_functions_basic.py
@@ -0,0 +1,13 @@
+# Functions in Python
+# Functions are pieces of code that can be called, they perform a certain task
+# We can use functions that are embeded in pythor or we can create our own function that we call later
+
+# def is a keyword which defines that its a function say_hi is the name, : - indicates the start of the function
+# all lines in a function need to be indented
+
+def say_hi():
+    print("Hello !")
+
+print("top")
+say_hi()  # calls the function and executes it
+print("bottom")
