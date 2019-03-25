ab9b39d97dbb4c077fee115e84f0aaa2295bced7
gozdur <gozdur.p@gmail.com>
2019-03-25 20:58:51 +0100

    return with print

---
+++ 12_if_statements_and comparisons.py
@@ -0,0 +1,13 @@
+
+
+# Comparisons this defines a function that uses if statements to compare numbers
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(300, 600, 67))
