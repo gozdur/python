4dd782232019858cb69b28ac4b0e5b928c910a56
gozdur <gozdur.p@gmail.com>
2019-03-27 22:34:41 +0100

    new file created from the script

--- Python_Basics/24_modules in python.py
+++ Python_Basics/24_modules in python.py
@@ -11,3 +11,12 @@ print(useful_tools.)
 #  external modules are stored in the same folder that the python is installed
 
 # external modules is stored in the lib/ folder with a name module_name.py when present there pycharm ide will be able to autocomplete functions
+
+
+# pip unstall is used to install external modules
+# pip uninstall is used to remove a module
+
+# modules not handeled by pip should have installation instructions
+
+
+# CAUTION of manually installing stuff that other people wrote
