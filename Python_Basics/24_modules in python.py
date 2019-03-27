d0a0d08a6a479ce4eb85b45d8f276b6dc9315618
gozdur <gozdur.p@gmail.com>
2019-03-27 22:24:17 +0100

    new file created from the script

---
+++ Python_Basics/24_modules in python.py
@@ -0,0 +1,13 @@
+import useful_tools   # imports functions written in outher python files
+
+print(useful_tools.roll_dice(10))
+
+print(useful_tools.feet_in_mile)
+
+print(useful_tools.)
+
+# in this example the module file was in the same directory as the python program
+#  two types of modules exist built in and external,
+#  external modules are stored in the same folder that the python is installed
+
+# external modules is stored in the lib/ folder with a name module_name.py when present there pycharm ide will be able to autocomplete functions
