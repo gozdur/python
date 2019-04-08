4b8e54c89aaed28751e088b39c4bc7f86993342d
a165026 <smoczeKULE7>
2019-04-08 12:50:47 +0200

    new commit

---
+++ Python_Basics/app.py
@@ -0,0 +1,8 @@
+
+# Here we import a student class from file student
+
+from student import student
+
+student1 = student("Jim", "Business", 3.5, False)  # We create an object student which gives values to the class student
+
+print(student1.name)
---
+++ Python_Basics/student.py
@@ -0,0 +1,10 @@
+
+
+
+
+class student:
+    def __init__(self, name, major, gpa, probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.probation = probation
